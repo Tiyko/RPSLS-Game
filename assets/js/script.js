4d26e470e6c4b68fc12e5e5bdbd33cdb6aa80dec
Tiyko <johnny86shepherd@gmail.com>
2022-04-28 21:10:37 +0000

    Add game function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,28 @@ let originalVersusText = document.getElementById('versus').innerText;
  */
 function runGame(user) {
 
+    // variable that calculates a random number up to 5 to represent the computer's choice
+    let com = Math.floor(Math.random() * 5);
+
+    // else if statements that compare the user's choice against the computer's choice
+    // and displays conclusion to the DOM
+    if (conclusion[user][com] == 1) {
+        // ads the comparison result to ID versus
+        document.getElementById('versus').innerText = 'WON ' + originalVersusText;
+    } else if (conclusion[user][com] == 2) {
+        // ads the comparison result to ID versus
+        document.getElementById('versus').innerText = 'LOST ' + originalVersusText;
+    } else {
+        // ads the comparison result to ID versus
+        document.getElementById('versus').innerText = 'TIED ' + originalVersusText;
+    }
+
+    // variables that store and write user and computer choice to the DOM
+    let userPick = document.getElementById('challenger-one');
+    userPick.innerText = choices[user];
+    let comPick = document.getElementById('challenger-two');
+    comPick.innerText = choices[com];
+
 }
 
 /**
