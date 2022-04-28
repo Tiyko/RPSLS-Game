b158490fdceb5cf0a8c62c5decc61a547c08fbae
Tiyko <johnny86shepherd@gmail.com>
2022-04-28 21:02:24 +0000

    Add initial structure for game functions.

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+/**
+ * variables with arrays that contain the game
+ * choices array and the matrice array
+ */
+let choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+let conclusion = [
+    [0, 2, 1, 1, 2],
+    [1, 0, 2, 2, 1],
+    [2, 1, 0, 1, 2],
+    [2, 1, 2, 0, 1],
+    [1, 2, 1, 2, 0]
+];
+
+// variable that stores the initial result text, to be added to the result when game is played.
+let originalVersusText = document.getElementById('versus').innerText;
+
+/**
+ * Function that runs the game
+ */
+function runGame(user) {
+
+}
+
+/**
+ * function that keeps track of score
+ */
+function gameScore(gameResult) {
+
+}
+
+/**
+ * switch function that changes game challengers background color
+ */
+function challengerColors(userPick) {
+
+}
+
+/**
+ * function that displays end game message
+ */
+function endGameMessage(result) {
+
+}
+
+/**
+ * function that resets the score at 10 
+ */
+function gameOver(oldscore) {
+
+}
+
+/**
+ * function that hides the game-over element on click
+ */
+function playAgain() {
+
+}
