0395f99ef76a037f459dea5d4597f5f255fc9890
Tiyko <johnny86shepherd@gmail.com>
2022-04-28 21:20:48 +0000

    Add function that keeps track of game score.

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,17 +22,23 @@ function runGame(user) {
     // variable that calculates a random number up to 5 to represent the computer's choice
     let com = Math.floor(Math.random() * 5);
 
+    // variable that stores the game result
+    let gameResult = '';
+
     // else if statements that compare the user's choice against the computer's choice
     // and displays conclusion to the DOM
     if (conclusion[user][com] == 1) {
         // ads the comparison result to ID versus
         document.getElementById('versus').innerText = 'WON ' + originalVersusText;
+        gameResult = 'WON';
     } else if (conclusion[user][com] == 2) {
         // ads the comparison result to ID versus
         document.getElementById('versus').innerText = 'LOST ' + originalVersusText;
+        gameResult = 'LOST';
     } else {
         // ads the comparison result to ID versus
         document.getElementById('versus').innerText = 'TIED ' + originalVersusText;
+        gameResult = 'TIED';
     }
 
     // variables that store and write user and computer choice to the DOM
@@ -41,12 +47,46 @@ function runGame(user) {
     let comPick = document.getElementById('challenger-two');
     comPick.innerText = choices[com];
 
+    // called function to display the score
+    gameScore(gameResult);
 }
 
 /**
  * function that keeps track of score
  */
 function gameScore(gameResult) {
+
+    // else if statements that compares the stored result in the runGame function
+    // and displays the result to the DOM
+    if (gameResult === 'WON') {
+
+        // increases user wins score by one to ID user-wins
+        let oldscore = parseInt(document.getElementById("user-wins").innerText);
+        document.getElementById("user-wins").innerText = ++oldscore;
+
+        // variable that changes background color
+        let wins = [...document.querySelectorAll("#versus, #user-wins")];
+        wins.map(element => element.style.backgroundColor = 'green');
+
+    } else if (gameResult === 'LOST') {
+
+        // increases computer wins score by one to ID com-wins
+        let oldscore = parseInt(document.getElementById("com-wins").innerText);
+        document.getElementById("com-wins").innerText = ++oldscore;
+
+        // variable that changes background color
+        let loss = [...document.querySelectorAll("#versus, #com-wins")];
+        loss.map(element => element.style.backgroundColor = 'red');
+
+    } else {
+        // increases ties score by one to ID ties
+        let oldscore = parseInt(document.getElementById("ties").innerText);
+        document.getElementById("ties").innerText = ++oldscore;
+
+        // variable that changes background color
+        let ties = [...document.querySelectorAll("#versus, #ties")];
+        ties.map(element => element.style.backgroundColor = 'rosybrown');
+    }
 
 }
 
